f1e70ed1e446b4717ef51287a415f698cf1b88ec
Chen An <v-chenan1@microsoft.com>
2021-11-16 08:49:03 +0000

    1

---
+++ ConsoleApp19/Program - Copy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //123
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
